921a0d84dd5317e974fed96cd7820ade7c0dba86
Cornelius <cornelius.braun@posteo.de>
2021-05-23 18:42:40 +0200

    added the condition to include effectors or not

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -34,25 +34,42 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         self.transforms = {n: np.eye(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
+        self.chains_with = {'Head': ['HeadYaw', 'HeadPitch'],
+                            # YOUR CODE HERE
+                            'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll', 'LWristYaw',
+                                     'LHand'],
+                            'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll', 'RWristYaw',
+                                     'RHand'],
+                            'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch',
+                                     'LAnkleRoll'],
+                            'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll']
+                            }
+
         self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
                        # YOUR CODE HERE
-                       'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll', 'LWristYaw', 'LHand'],
-                       'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll', 'RWristYaw', 'RHand'],
-                       'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
-                       'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll']
+                       'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll'],
+                       'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll'],
+                       'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch',
+                                'LAnkleRoll'],
+                       'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch',
+                                'RAnkleRoll']
                        }
-        self.joint_lengths = {'HeadYaw': (0., 0., .1265), 'HeadPitch': (0., 0., 0.),
-                              'LShoulderPitch': (0., .098, .100), 'LShoulderRoll': (0., 0., 0.),
-                              'LElbowYaw': (.105, .015, 0.), 'LElbowRoll': (0., 0., 0.),
-                              'RShoulderPitch': (0., .098, .1), 'RShoulderRoll': (0., 0., 0.),
-                              'RElbowYaw': (.105, .015, 0.), 'RElbowRoll': (0., 0., 0.),
-                              'LHipYawPitch': (0., .050, -.085), 'RHipYawPitch': (0., .050, -.085),
-                              'LHipRoll': (0., 0., 0.), 'LHipPitch': (0., 0., 0.), 'LKneePitch': (0., 0., -.100),
-                              'LAnklePitch': (0., 0., -.1029), 'LAnkleRoll': (0., 0., 0.),
-                              'RHipRoll': (0., 0., 0.), 'RHipPitch': (0., 0., 0.), 'RKneePitch': (0., 0., -.100),
-                              'RAnklePitch': (0., 0., -.1029), 'RAnkleRoll': (0., 0., 0.), 'RWristYaw': (.05595, 0., 0.),
-                              'LHand': (.05775, 0., .01231), 'RHand': (.05775, 0., .01231), 'LWristYaw': (.05595, 0., 0.)
+
+        self.joint_lengths = {'HeadYaw': (0., 0., 126.5), 'HeadPitch': (0., 0., 0.),
+                              'LShoulderPitch': (0., 98., 100.), 'LShoulderRoll': (0., 0., 0.),
+                              'LElbowYaw': (105., 15., 0.), 'LElbowRoll': (0., 0., 0.),
+                              'RShoulderPitch': (0., -98., 100.), 'RShoulderRoll': (0., 0., 0.),
+                              'RElbowYaw': (105., -15., 0.), 'RElbowRoll': (0., 0., 0.),
+                              'LHipYawPitch': (0., 50., -85.), 'RHipYawPitch': (0., -50., -85.),
+                              'LHipRoll': (0., 0., 0.), 'LHipPitch': (0., 0., 0.), 'LKneePitch': (0., 0., -100.),
+                              'LAnklePitch': (0., 0., -102.9), 'LAnkleRoll': (0., 0., 0.),
+                              'RHipRoll': (0., 0., 0.), 'RHipPitch': (0., 0., 0.), 'RKneePitch': (0., 0., -100.),
+                              'RAnklePitch': (0., 0., -102.9), 'RAnkleRoll': (0., 0., 0.),
+                              'LHand': (57.75, 0., 12.31), 'RHand': (57.75, 0., 12.31),
+                              'LWristYaw': (55.95, 0., 0.), 'RWristYaw': (55.95, 0., 0.)
                               }
+
+        self.end_effectors = ['LHand', 'RHand', 'LWristYaw', 'RWristYaw']
 
     def think(self, perception):
         self.forward_kinematics(perception.joint)
@@ -92,7 +109,7 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
             T = T @ rot_y
         if 'Yaw' in joint_name:
             T = T @ rot_z
-        return T
+        return T.copy()
 
     def forward_kinematics(self, joints):
         '''forward kinematics
@@ -100,14 +117,15 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         :param joints: {joint_name: joint_angle}
         '''
         for chain_joints in self.chains.values():
+        # for chain_joints in self.chains_with.values():
             T = np.eye(4)
             for joint in chain_joints:
-                angle = 0 if joint in ['RWristYaw', 'LWristYaw', 'LHand', 'RHand'] else joints[joint]
-                # angle = joints[joint]
+                # angle = joints[joint] if joint not in self.end_effectors else 0
+                angle = joints[joint]
                 Tl = self.local_trans(joint, angle)
                 # YOUR CODE HERE
                 T = T @ Tl
-                self.transforms[joint] = T
+                self.transforms[joint] = T.copy()
 
 
 if __name__ == '__main__':
